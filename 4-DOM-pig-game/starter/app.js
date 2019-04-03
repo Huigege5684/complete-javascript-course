02f79db7a2e64dfc6f7ae7f6291cdc5370b58cc9
Will Lin <ylin@zenefits.com>
2019-04-02 17:42:13 -0700

    first commit

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -1,4 +1,4 @@
-/*
+/* 
 GAME RULES:
 
 - The game has 2 players, playing in rounds
@@ -8,3 +8,56 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+let scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = scores[0];
+document.getElementById('score-1').textContent = scores[0];
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+	let dice = Math.floor(Math.random() * 6) + 1;
+
+	let diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+
+	if (dice != 1) {
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		nextPlayer();
+	}
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+	scores[activePlayer] += roundScore;
+
+	document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+	if (scores[activePlayer] >= 20) {
+		document.querySelector('#name-' + activePlayer).textContent = 'Winner !';
+		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+	} else {
+		nextPlayer();
+	}
+});
+
+function nextPlayer() {
+	roundScore = 0;
+	document.querySelector('#current-' + activePlayer).textContent = 0;
+
+	document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+	activePlayer = 1 - activePlayer;
+	document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+
+	diceDOM.style.display = 'none';
+}
